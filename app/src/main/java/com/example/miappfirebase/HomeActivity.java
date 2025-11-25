7d2518912cf26c2ba753a1a5272cdd9409382841
Sebastián Cabezas Ríos <sebastian.cabezas.r@gmail.com>
2025-11-25 20:21:49 -0300

    demo2

--- app/src/main/java/com/example/miappfirebase/HomeActivity.java
+++ app/src/main/java/com/example/miappfirebase/HomeActivity.java
@@ -1,6 +1,10 @@
 package com.example.miappfirebase;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,77 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private Button btnObtener, btnONOFF, btnLogout;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btnObtener = findViewById(R.id.btnObtener);
+        btnONOFF = findViewById(R.id.btnONOFF);
+        btnLogout = findViewById(R.id.btnLogout);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser == null){
+            irALogin();
+            return;
+        }
+
+        btnObtener.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomeActivity.this, "Obteniendo datos...", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 mAuth.signOut();
+                 irALogin();
+             }
+         });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    private void irALogin(){
+        Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
